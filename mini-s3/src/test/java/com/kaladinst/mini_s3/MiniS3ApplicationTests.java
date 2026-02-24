be46f26e37f48b340d8a6cd0812f3a06b9ca7880
emir <emir.d10@outlook.com>
2026-02-24 21:06:28 +0300

    Initial commit: Spring Boot base project setup with dependencies

---
+++ mini-s3/src/test/java/com/kaladinst/mini_s3/MiniS3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaladinst.mini_s3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniS3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
